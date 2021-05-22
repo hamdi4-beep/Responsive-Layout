3575dba35ad80ee0b89b4a3cf803e13da1a2a03c
hamdi-beep4 <git@github.com>
2021-05-22 12:00:08 -0700

    added some fancy animation

---
+++ script.js
@@ -0,0 +1,23 @@
+const title = document.querySelector('.title')
+const container = title.parentElement
+const str = title.textContent
+let char = 0
+title.textContent = ''
+
+for (const letter of str) {
+    const span = document.createElement('span')
+    span.textContent = letter
+    title.append(span)
+}
+
+const spans = document.querySelectorAll('span')
+
+const timer = setInterval(
+    () => {
+        if (char < spans.length) {
+            spans[char++].classList.add('fade-in')
+        } else {
+            clearInterval(timer)
+        }
+    }
+, 100)
