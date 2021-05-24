8fe9aec726b967e2d7942ebd420b34f86da222ba
hamdi-beep4 <git@github.com>
2021-05-23 17:44:15 -0700

    added more content and improved on the design

--- script.js
+++ script.js
@@ -21,20 +21,3 @@ const timer = setInterval(
         }
     }
 , 200)
-
-fetch('https://api.github.com/users/hamdi4-beep').then(res => {
-    if (res.ok) {
-        const body = res.json();
-        body.then(data => {
-            const imgWrapper = document.createElement('div')
-            const img = document.createElement('img')
-            imgWrapper.classList.add('avatar-wrapper')
-            img.src = data.avatar_url
-            imgWrapper.append(img)
-            container.prepend(imgWrapper)
-            container
-        })
-    } else {
-        throw new Error('Could not fetch data from this server')
-    }
-})
