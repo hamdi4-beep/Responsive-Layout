b714a62038270c76dd1ebf7041909d66f8c8a1c5
hamdi-beep4 <git@github.com>
2021-05-28 08:50:55 -0700

    improved the JS code loading times

--- scripts/script.js
+++ scripts/script.js
@@ -1,36 +1,40 @@
-const title = document.querySelector('.title') // the username element
-const left = document.querySelector('.left')
-const right = document.querySelector('.right')
-const checkbox = document.querySelector('input') // the checkbox element
-const str = title.textContent // the username's string
-let char = 0
-title.textContent = ''
+window.onload = init
 
-// a toggle switch functionality for the checkbox
+function init() {
+    const title = document.querySelector('.title') // the username element
+    const left = document.querySelector('.left')
+    const right = document.querySelector('.right')
+    const checkbox = document.querySelector('input') // the checkbox element
+    const str = title.textContent // the username's string
+    let char = 0
+    title.textContent = ''
 
-checkbox.addEventListener('change', e => {
-    const elem = e.target // the check box
-    if (elem.checked) {
-        left.classList.add('dark-mode')
-    } else {
-        left.classList.remove('dark-mode')
-    }
-})
+    // a toggle switch functionality for the checkbox
 
-for (const letter of str) {
-    const span = document.createElement('span')
-    span.textContent = letter
-    title.append(span)
-}
-
-const spans = document.querySelectorAll('span')
-
-const timer = setInterval(
-    () => {
-        if (char < spans.length) {
-            spans[char == 5 ? char += 1 : char++].classList.add('fade-in')
+    checkbox.addEventListener('change', e => {
+        const elem = e.target // the check box
+        if (elem.checked) {
+            left.classList.add('dark-mode')
         } else {
-            clearInterval(timer)
+            left.classList.remove('dark-mode')
         }
+    })
+
+    for (const letter of str) {
+        const span = document.createElement('span')
+        span.textContent = letter
+        title.append(span)
     }
-, 100)
+
+    const spans = document.querySelectorAll('span')
+
+    const timer = setInterval(
+        () => {
+            if (char < spans.length) {
+                spans[char == 5 ? char += 1 : char++].classList.add('fade-in')
+            } else {
+                clearInterval(timer)
+            }
+        }
+    , 100)
+}
